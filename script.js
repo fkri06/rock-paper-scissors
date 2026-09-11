1d6646c8949b89e0077e26d6574b7a9c45111061
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-11 11:08:38 +0700

    Finish a playring round logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     const getRandomValue = Math.floor(Math.random() * 3);
     switch (getRandomValue) {
@@ -16,3 +19,21 @@ function getHumanChoice () {
     let humanChoice = prompt("Choose rock, paper, scissors: ");
     return humanChoice.toLowerCase();
 } 
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Ties.");
+    } else if ((humanChoice === "paper" && computerChoice === "rock") || 
+         (humanChoice === "rock" && computerChoice === "scissors") ||
+         (humanChoice === "scissors" && computerChoice === "paper")) 
+    {
+        console.log (`You win! ${humanChoice} beats ${computerChoice}.`);
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
