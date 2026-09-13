fcd5c9cc2fb389e43eb2655f79805d10bd2cb4da
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-13 20:06:45 +0700

    Finish refactor the script
    - Add rounds variable to count the rounds
      in each game.
    - Add divs to show the status of game in each
      round and the final score.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+let rounds = 0;
+
 function getComputerChoice () {
     const getRandomValue = Math.floor(Math.random() * 3);
     switch (getRandomValue) {
@@ -15,39 +19,67 @@ function getComputerChoice () {
 function getHumanChoice () {
     let humanChoice = prompt("Choose rock, paper, scissors: ");
     return humanChoice.toLowerCase();
-} 
-
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-
-    let round = 5;
-
-    while (round !== 0) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        
-        if (humanChoice === computerChoice) {
-            console.log("Ties.");
-        } else if ((humanChoice === "paper" && computerChoice === "rock") || 
-         (humanChoice === "rock" && computerChoice === "scissors") ||
-         (humanChoice === "scissors" && computerChoice === "paper")) 
-        {
-            console.log (`You win! ${humanChoice} beats ${computerChoice}.`);
-            humanScore++;
-        } else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-            computerScore++;
-        }
-        round--;
-    }
-
-    console.log("Final score: ");
-    console.log(`Human: ${humanScore}`);
-    console.log(`Computer: ${computerScore}`);
-
 }
 
-playGame();
+function playRound(humanSelection) {
+    const status = document.querySelector(".status");
+    const humanChoice = humanSelection.toLowerCase();
+    const computerChoice = getComputerChoice();
+    
+    if (humanChoice === computerChoice) {
+        status.textContent = `It's a tie! You pick ${humanChoice} and computer pick ${computerChoice}`;
+    } else if ((humanChoice === "paper" && computerChoice === "rock") || 
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) 
+    {
+        status.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+        humanScore++;
+    } else {
+        status.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+        computerScore++;
+    }
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", function(event) {
+    rounds++;
+    
+    // if status div does not exist yet
+    if (!document.querySelector(".status")) {
+        const body = document.body;
+        const status = document.createElement("div");
+        status.classList.add("status");
+        body.appendChild(status);        
+    }
+
+    playRound(event.target.innerText);
+
+    if (rounds === 5) {
+        const status = document.querySelector(".status");
+        if (humanScore > computerScore) {
+            status.textContent = "Congratulations you win the game!";
+        } else if (computerScore > humanScore) {
+            status.textContent = "Computer wins!";
+        } else {
+            status.textContent = "It was a tie game...";
+        }
+
+        const finalScoreDiv = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const humanP = document.createElement("p");
+        const computerP = document.createElement("p");
+
+        finalScoreDiv.appendChild(h2);
+        finalScoreDiv.appendChild(humanP);
+        finalScoreDiv.appendChild(computerP);
+        
+        h2.textContent = "Final Score: ";
+        humanP.textContent = `Human: ${humanScore}`;
+        computerP.textContent = `Computer: ${computerScore}`;
+        
+        document.body.appendChild(finalScoreDiv);
+
+        // reset rounds and scores
+        rounds = humanScore = computerScore = 0;
+    }
+}));
